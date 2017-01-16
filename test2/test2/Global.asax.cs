879cba8fbed5355e8b2cdc4484761f85fbb0fa98
Thomas Gerdes <gerdes-thomas@gmx.de>
2017-01-16 15:01:00 +0100

    0

--- test2/test2/Global.asax.cs
+++ test2/test2/Global.asax.cs
@@ -16,6 +16,8 @@ namespace test2
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
+
         }
     }
 }
